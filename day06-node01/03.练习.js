7c187f476360bdc1a4e03fa3d8711034139aaea3
nowLetsgo <lipeihua0922@163.com>
2021-10-10 11:43:01 +0800

    day06-nodejs

--- day06-node01/03.练习.js
+++ day06-node01/03.练习.js
@@ -168,7 +168,7 @@ Promise.resolve().then(function () {
 
 //test9
 //1 3 4 2
-Promise.resolve().then(() => {
+/* Promise.resolve().then(() => {
     console.log(1);
     process.nextTick(() => {
         console.log(2);
@@ -181,31 +181,142 @@ Promise.resolve().then(function () {
     }).catch(function () {
         console.log(4);
     });
-});
+}); */
 
 
 //test10
-new Promise((resolve, reject) => {
-    console.log(1)
-    resolve(1)
-})
-.then(() => {
-    console.log(2)
-    new Promise((resolve, reject) => {
-            console.log(3)
-            resolve()
-        })
-        .then(() => {
-            console.log(4)
-        })
-        .then(() => {
-            console.log(5)
-        })
-        .then(() => {
-            console.log(6)
-        })
-    console.log(8);
-})
-.then(() => {
-    console.log(7)
-})
+//1 2 3 8 4 7 5 6
+/* new Promise((resolve, reject) => {
+        console.log(1)
+        resolve(1)
+    })
+    .then(() => { //then1
+        console.log(2)
+        new Promise((resolve, reject) => {
+                console.log(3)
+                resolve()
+            })
+            .then(() => { //then2
+                console.log(4)
+            })
+            .then(() => { //then3
+                console.log(5)
+            })
+            .then(() => { //then4
+                console.log(6)
+            })
+        console.log(8);
+    })
+    .then(() => { //then5
+        console.log(7)
+    }) */
+
+//test11
+//1 2 9 4 10 13 3 15 16 5 6 8 7
+/* console.log(1); //
+new Promise(function (resolve) {
+    resolve();
+    console.log(2); //
+    setTimeout(function () {//定1
+        console.log(3); //
+    }, 0);
+    Promise.resolve().then(function () {//then1
+        console.log(4); //
+        setTimeout(function () {//定3
+            console.log(5);
+        }, 0);
+        setTimeout(function () {//定4
+            (async function () {
+                console.log(6);
+                return function () {
+                    console.log(7);
+                };
+            })().then(function (fn) {
+                console.log(8);
+                fn();
+            });
+        }, 0);
+    });
+    new Promise(function (resolve) {
+        console.log(9); //
+        resolve();
+    }).then(function () {//then2
+        new Promise(function (resolve, reject) {
+            console.log(10);
+            reject();
+        }).then(function () {
+            setTimeout(function () {
+                console.log(11);
+            }, 0);
+            console.log(12);
+        }).catch(function () {//then3
+            console.log(13);
+        });
+    });
+});
+setTimeout(function () {//定2
+    console.log(15);
+    Promise.resolve().then(function () {//then4
+        console.log(16);
+    });
+}, 0); */
+
+
+//test12
+console.log(1); //
+new Promise((res, rej) => {
+    console.log(2); //
+    res();
+}).then(() => {//then1
+    console.log(3); //
+    Promise.resolve().then(() => {//then3
+        console.log(5);
+        setTimeout(function () {//定5
+            console.log(6); //
+            Promise.resolve().then(function () {
+                console.log(7);
+            });
+            setTimeout(function () {//定7
+                console.log(8);
+            }, 0);
+        }, 0);
+    });
+}).then(() => {//then4
+    console.log(4);
+});
+
+new Promise((res) => {
+    console.log(19); //
+    setTimeout(() => {//定1
+        console.log(20); //
+    }, 0);
+});
+Promise.resolve().then(() => {//then2
+    setTimeout(() => {//定4
+        Promise.resolve().then(() => {
+            console.log(12);
+        });
+        console.log(13); //
+    }, 0);
+});
+setTimeout(() => {//定2
+    console.log(9); //
+    new Promise((res) => {
+        res();
+        console.log(10);
+    }).then(() => {//then5
+        console.log(11);
+    });
+});
+setTimeout(() => {//定3
+    setTimeout(() => {//定6
+        setTimeout(() => {//定8
+            Promise.resolve().then(() => {
+                console.log(14);
+            });
+            console.log(15);
+        }, 0);
+        console.log(16);
+    }, 0);
+    console.log(17); //
+}, 0);
