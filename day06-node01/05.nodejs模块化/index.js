9e93dfb0a615e161a13c50016d63a0fdab7f7ead
nowLetsgo <lipeihua0922@163.com>
2021-10-10 16:52:47 +0800

    day06-over

--- day06-node01/05.nodejs模块化/index.js
+++ day06-node01/05.nodejs模块化/index.js
@@ -1,12 +1,27 @@
 //引入模块
 //1.当暴露的是{add:fn}
-const {
+/* const {
     add
 } = require("./add");
 console.log(add);
-console.log(add(1, 2));
+console.log(add(1, 2)); */
 
 //2.当暴露的是add的时候
 /* const add = require("./add");
 console.log(add);
 console.log(add(1, 1)); */
+
+
+/* 
+    模块标识：
+        - require()中书写引入的模块内容，叫做模块标识
+        - 模块标识分为3中
+            - 核心模块（node自带模块 直接引入即可） require('fs')
+            - 第三方模块(别人写好的模块，我们下载之后 直接引入) npm i jquery  / require("jquery")
+            - 自定义模块(我们自己书写的模块,直接按照路径引入，可以省略后缀)(省略后缀自动会按照以下顺序添加：.js .json .node)
+
+*/
+
+
+const fs = require("fs");
+console.log(fs.open);
---
+++ npm命令.md
@@ -0,0 +1,20 @@
+- npm init :初始化一个package.json
+- npm i jquery / npm i jquery --save /npm i jquery -S : 下载生产依赖的包
+- npm i jquery --save-dev /npm i jquery -D :下载开发依赖的包
+- npm i :下载所有package.json中依赖的包
+- npm i jquery@1:下载1版本的最新版
+- npm i jquery@1.1:下载1.1版本的最新版
+- npm i jquery@1.1.1:下载1.1.1版本
+- npm r jquery:删除jquery的包
+
+- npm i -g nodemon:全局安装某个包（全局的安装基本都是命令）
+
+
+
+- npm i yarn -g:下载yarn工具
+- yarn init -y :初始化package.json
+- yarn add jquery:下载生产依赖的包
+- yarn add jquery -D:下载开发依赖的包
+- yarn global add less:全局安装less工具
+- yarn remove jquery:删除某个包
+- yarn：下载所有依赖
