a2e3396fc0a30301efc705044d3eac2a1880ec45
nowLetsgo <lipeihua0922@163.com>
2021-10-11 16:40:19 +0800

    day07-over

---
+++ day07-node核心模块/07.http/02.作为客户端.js
@@ -0,0 +1,23 @@
+const http = require("http");
+
+//设置请求地址：
+const url = "http://192.168.19.35:3000"
+//设置请求
+const request = http.request(url, response => {
+    //回调函数的参数response 就是响应的对象
+
+    // console.log(response);
+    //响应状态码
+    console.log(response.statusCode);
+
+    //node作为客户端请求到数据是可读流，需要使用可读流的是data事件来接受
+    response.on("data", chunk => {
+        console.log(chunk);
+        console.log(chunk.toString());
+    })
+
+});
+
+
+//发送请求
+request.end();
