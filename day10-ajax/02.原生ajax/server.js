d7bce6244f8fa401d3434ee6d455eab8815d7a71
nowLetsgo <lipeihua0922@163.com>
2021-10-15 14:31:08 +0800

    day10-ajax

--- day10-ajax/02.原生ajax/server.js
+++ day10-ajax/02.原生ajax/server.js
@@ -4,9 +4,16 @@ const path = require("path")
 
 //登录页面
 app.get("/", (req, res) => {
-    const filePath = path.resolve(__dirname, '01.get请求.html');
+    // const filePath = path.resolve(__dirname, '01.get请求.html');
+    const filePath = path.resolve(__dirname, '02.post请求.html');
     res.sendFile(filePath)
 })
+
+//处理post请求报文体的中间件（把报文体的内容放在req.body上）
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json());
 
 //登录get接口
 app.get("/login", (req, res) => {
@@ -15,6 +22,25 @@ app.get("/login", (req, res) => {
     console.log(query);
     //判断用户名和密码是否正确
     if (query.user === "laoli" && query.pass === '123456') {
+        return res.json({
+            code: 100001,
+            msg: "登录成功"
+        })
+    }
+
+    res.send({
+        code: 100000,
+        msg: "登录失败"
+    })
+})
+
+//登录get接口
+app.post("/login", (req, res) => {
+    //拿到查询字符串
+    const body = req.body;
+    console.log(body);
+    //判断用户名和密码是否正确
+    if (body.user === "laoli" && body.pass === '123456') {
         return res.json({
             code: 100001,
             msg: "登录成功"
