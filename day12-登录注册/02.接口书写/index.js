50d4e8275d2e46ec327ebcb46a14738a5343c931
nowLetsgo <lipeihua0922@163.com>
2021-10-18 14:33:42 +0800

    day12-登录注册

--- day12-登录注册/02.接口书写/index.js
+++ day12-登录注册/02.接口书写/index.js
@@ -6,6 +6,9 @@ const path = require("path")
 require("./db")
 const userModel = require("./models/userModel");
 
+//引入md5模块
+const md5 = require("md5");
+
 //1.官方的静态资源中间件（服务器上的文件响应）
 //暴露views中的静态文件，直接可以在服务器的地址栏后添加对应的静态资源路径即可访问
 app.use(express.static(path.resolve(__dirname, "./views")))
@@ -36,12 +39,15 @@ app.post("/register", async (req, res) => {
         user,
         pass
     } = req.body;
+    // console.log(user, pass)
 
     //根据当前的user 去数据库查询是否有该用户名存在
     //如果查询到，则响应当前查询的到文档组成的对象,否则响应一个null
     const findResult = await userModel.findOne({
         username: user
     })
+    // console.log(findResult)
+
 
     //如果查询存在当前用户名(也就是findResult是个对象)，则直接响应 用户名已经存在的结果
     if (findResult) {
@@ -54,7 +60,7 @@ app.post("/register", async (req, res) => {
     //当查询的用户名不存在(也就是findResult是null的时候)，则向数据库添加数据
     const createResult = await userModel.create({
         username: user,
-        password: pass,
+        password: md5(pass),
         createTime: new Date()
     })
 
@@ -64,6 +70,55 @@ app.post("/register", async (req, res) => {
         msg: "注册成功"
     })
 
+})
+
+
+//登录接口
+/* 
+    接口文档：
+        url:"/login",
+        method:"POST",
+        请求字段：
+            user:用户名 Str
+            pass:密码 Str
+        响应字段：
+            code：状态代码 10000:成功 10001：失败
+            msg：响应状态
+*/
+
+app.post("/login", async (req, res) => {
+    //拿到请求数据
+    const {
+        user,
+        pass
+    } = req.body;
+
+    //去数据库查找是否有该用户
+    const findResult = await userModel.findOne({
+        username: user
+    })
+
+    //如果数据库没有该用户 则findResult 返回null
+    if (!findResult) {
+        return res.json({
+            code: 10001,
+            msg: "用户名不存在"
+        })
+    }
+
+    //如果存在该用户，则findResult就是查询的结果，判断用户输入的密码和结果是否一致
+    if (md5(pass) !== findResult.password) {
+        return res.json({
+            code: 10002,
+            msg: "密码错误"
+        })
+    }
+
+    //登录成功
+    res.json({
+        code: 10000,
+        msg: "登录成功"
+    })
 })
 
 
