b505190e2ee0d80942941e3516ff18b671a1e1f7
nowLetsgo <lipeihua0922@163.com>
2021-10-18 10:50:11 +0800

    day12-登录注册

---
+++ day12-登录注册/02.接口书写/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+//连接数据库
+mongoose.connect("mongodb://127.0.0.1:27017/goat0722");
+
+//当数据库连接成功会触发mongoose.connection对象的open事件
+mongoose.connection.once("open", err => {
+    if (err) {
+        console.log("数据库连接失败", err);
+        return;
+    }
+    console.log("数据库连接成功");
+})
