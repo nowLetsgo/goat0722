93e92e4c053f97ab7c27a97d4615a16d0789239d
nowLetsgo <lipeihua0922@163.com>
2021-10-22 15:30:39 +0800

    day15-webpack

--- day15-webpack/webpack.config.js
+++ day15-webpack/webpack.config.js
@@ -10,23 +10,34 @@ module.exports = {
         //输出配置的文件路径及文件名 路径相对于path
         filename: "./js/main.js",
         //webpack打包的参考路径
-        path: path.resolve(__dirname, "build")
+        path: path.resolve(__dirname, "build"),
+        assetModuleFilename: "./images/[hash:8][ext][query]",
     },
     //loader配置
     module: {
         rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"],
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
                 },
+            }, {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
             },
-        }, {
-            test: /\.less$/,
-            use: ["style-loader", "css-loader", "less-loader"]
-        }],
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                type: "asset",
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 15 * 1024, // 小于15kb以下的图片会被打包成base64格式
+                    },
+                },
+            }
+        ],
     },
     //插件配置
     plugins: [new ESLintPlugin()],
